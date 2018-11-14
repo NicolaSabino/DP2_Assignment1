a29e4f0312e5164c972042578467535652405376
Nicola <nicola.sabino94@gmail.com>
2018-11-14 10:25:39 +0100

    fixing factory and monitor in the constructor

--- src/it/polito/dp2/RNS/sol1/RnsInfoSerializer.java
+++ src/it/polito/dp2/RNS/sol1/RnsInfoSerializer.java
@@ -1,60 +1,100 @@
 package it.polito.dp2.RNS.sol1;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-
-import javax.xml.bind.JAXBContext;
-
+import java.util.Set;
+import it.polito.dp2.RNS.GateReader;
 import it.polito.dp2.RNS.RnsReader;
 import it.polito.dp2.RNS.RnsReaderException;
 import it.polito.dp2.RNS.RnsReaderFactory;
-import it.polito.dp2.RNS.lab1.RnsInfo;
-import it.polito.dp2.RNS.sol1.jaxb.*;
-import it.polito.dp2.RNS.sol1.jaxb.System;
 
 public class RnsInfoSerializer {
-	
-	private RnsReaderFactory factory;
+	private RnsReader 			monitor;
+	private RnsReaderFactory	factory;
+	//private DateFormat			dateFormat;
 
-	
-	public RnsInfoSerializer() {
+	/**
+	 * Constructor
+	 * @throws RnsReaderException 
+	 */
+	public RnsInfoSerializer() throws RnsReaderException {
 		// The implementation of the interfaces to be used as data source
 		// must be selected using the  `abstract factory` pattern:
 		// we must create the data source by instantiating 
 		// `it.polito.dp2.RNS.RnsReaderFactory` by means of its static method
 		// `newInstance()`
 		this.factory = RnsReaderFactory.newInstance();
+		this.monitor = factory.newRnsReader();
+		//this.dateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm");
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//instantiate the class
-		RnsInfoSerializer serializer;
-		String filename;
-		
+		// check if there is the file name
 		if (args.length == 0){
-            //no arguments
-            return;
+            //if there are no arguments exit the program
+			// we identify this error with ERRORCODE #1
+			System.out.println("No filename");
+            System.exit(1);
         }
-		
-		serializer = new RnsInfoSerializer();
+		System.err.println("The output file is:" + args[0]);
+		RnsInfoSerializer serializer;
 		
 		try {
-			JAXBContext jc = JAXBContext.newInstance(“it.polito.dp2.RNS.sol1.jaxb”);
-			// create JAXBElement object
-			JAXBElement j= ...;
-			Marshaller m = jc.createMarshaller();
-			m.setProperty( Marshaller.JAXB_FORMATTED_OUTPUT,
-			Boolean.TRUE );
-			m.marshal( j, System.out );
-			} catch( JAXBException je ) {
-			je.printStackTrace();
-			} catch( IOException ioe ) {
-			ioe.printStackTrace();
-			}
+			serializer = new RnsInfoSerializer();
+			serializer.createFile(args[0]);
+		} catch (RnsReaderException e) {
+			// An error occurs while the instantiation of the class
+			// we identify this error with ERRORCODE #2
+			System.err.println("Could not instantiate data generator.");
+			e.printStackTrace();
+			System.exit(2);
+		}
 		
+		
+	}
+	
+	private void createFile(String filename){
+		// Get the list of Gates
+				Set<GateReader> set = monitor.getGates(null);
+				
+				/* Print the header of the table */
+				printHeader('#',"#Information about GATES");
+				printHeader("#Number of Gates: "+set.size());
+				printHeader("#List of Gates:");
+				printHeader("Id"+"\tCapacity"+"\tType",'-');
+				
+				// For each Gate print related data
+				for (GateReader gate: set) {
+					printHeader(gate.getId()+"\t"+gate.getCapacity()+"\t"+gate.getType().name());
+				}
+	}
+	
+	private void printHeader(String header) {
+		System.out.println(header);
+	}
+
+	private void printHeader(String header, char c) {		
+		System.out.println(header);
+		printLine(c);	
+	}
+	
+	private void printHeader(char c, String header) {		
+		printLine(c);	
+		System.out.println(header);
+	}
+	
+	private void printLine(char c) {
+		System.out.println(makeLine(c));
+	}
+	
+	private StringBuffer makeLine(char c) {
+		StringBuffer line = new StringBuffer(132);
+		
+		for (int i = 0; i < 132; ++i) {
+			line.append(c);
+		}
+		return line;
 	}
 
 	

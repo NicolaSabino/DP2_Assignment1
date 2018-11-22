d8e059a2a875dcac0e974ba1c1de7dee03f1cd06
Nicola Sabino <s253839@labinf65.polito.it>
2018-11-22 16:52:50 +0100

    pre merge utilitymap

--- src/it/polito/dp2/RNS/sol1/RnsReader_.java
+++ src/it/polito/dp2/RNS/sol1/RnsReader_.java
@@ -32,91 +32,9 @@ public class RnsReader_ implements it.polito.dp2.RNS.RnsReader {
 	 */
 	public RnsReader_(RoadNavigationSystem rns) {
 		this.rns = rns;
-		storeIDEntities();
-		storePlaces();
-		storeVehicles();
 	}
-
 
 	
-
-
-	private void storeVehicles() {
-		//donor
-		List<it.polito.dp2.RNS.sol1.jaxb.VehicleType> places = this.rns.getVehicle();
-		//target
-		this.vehicles = new HashSet<VehicleReader_>();
-		//for each vehicle
-		for(it.polito.dp2.RNS.sol1.jaxb.VehicleType vehicle: vehicles){
-			String id = vehicle.getId();
-			//TODO fix these places
-			//String position = vehicle.getPosition();
-			//String origin = vehicle.getOrigin();
-			//String destination = vehicle.getDestination();
-			XMLGregorianCalendar cal = vehicle.getEntryTime();
-			String state = vehicle.getState().toString();
-			String type = vehicle.getType().toString();
-			
-			//conversions
-			it.polito.dp2.RNS.sol1.jaxb.VState s = it.polito.dp2.RNS.sol1.jaxb.VState.valueOf(state);
-			it.polito.dp2.RNS.sol1.jaxb.VType t = it.polito.dp2.RNS.sol1.jaxb.VType.valueOf(type);
-			VehicleReader_ tmp = new VehicleReader_(id,null, null, null, null,s, t); // TODO fix
-			this.vehicles.add(tmp);
-		}
-		// all the entities (places and vehicles) are stored in `this.entities
-		return;
-	}
-
-
-
-
-
-	private void storePlaces() {
-		//donor
-		List<it.polito.dp2.RNS.sol1.jaxb.PlaceType> places = this.rns.getPlace();
-		//target
-		this.places = new HashSet<PlaceReader_>();
-		//for each place
-		for(it.polito.dp2.RNS.sol1.jaxb.PlaceType place: places){
-			String id = place.getId();
-			int capacity = place.getCapacity();
-			List<String> next = place.getNextPlace();
-			PlaceReader_ tmp = new PlaceReader_(id,capacity,null); //TODO remove or fix `null`
-			this.places.add(tmp);
-		}
-		// all the places are stored in `this.entities
-		return;
-		
-	}
-
-
-
-
-	/**
-	 * Use in chain storePlaces() and storeVehicles()
-	 */
-	private void storeIDEntities() {
-		// donors
-		List<it.polito.dp2.RNS.sol1.jaxb.VehicleType> vehicles = this.rns.getVehicle();
-		List<it.polito.dp2.RNS.sol1.jaxb.PlaceType> places = this.rns.getPlace();
-		//target
-		this.entities = new HashSet<IdentifiedEntityReader_>();
-		
-		//for each vehicle
-		for(it.polito.dp2.RNS.sol1.jaxb.VehicleType vehicle: vehicles){
-			IdentifiedEntityReader_ entity = new IdentifiedEntityReader_(vehicle.getId());
-			entities.add(entity);
-		}
-		//for each place
-		for(it.polito.dp2.RNS.sol1.jaxb.PlaceType place: places){
-			IdentifiedEntityReader_ entity = new IdentifiedEntityReader_(place.getId());
-			entities.add(entity);
-		}
-		// all the entities (places and vehicles) are stored in `this.entities
-		return;
-	}
-
-
 	@Override
 	public Set<it.polito.dp2.RNS.ConnectionReader> getConnections() {
 		// TODO Auto-generated method stub

f4acf478e445f11bf78ea88175c67ba5184fa376
Nicola <nicola.sabino94@gmail.com>
2018-11-26 18:18:45 +0100

    navigable map and pruned maps

--- src/it/polito/dp2/RNS/sol1/RnsReader_.java
+++ src/it/polito/dp2/RNS/sol1/RnsReader_.java
@@ -1,7 +1,10 @@
 package it.polito.dp2.RNS.sol1;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 
@@ -16,20 +19,31 @@ public class RnsReader_ implements it.polito.dp2.RNS.RnsReader {
 	 * @param rns 
 	 */
 	public RnsReader_(it.polito.dp2.RNS.sol1.jaxb.RoadNavigationSystem rns) {
-		//this.rns = rns;
 		this.utility = new UtilityMap(rns);
 	}
 
 	@Override
 	public Set<it.polito.dp2.RNS.ConnectionReader> getConnections() {
-		// TODO Auto-generated method stub
-		return null;
+		Set<it.polito.dp2.RNS.ConnectionReader> result_set = new HashSet<>(this.utility.c_list);
+		return result_set;
 	}
 
 	@Override
 	public Set<it.polito.dp2.RNS.GateReader> getGates(it.polito.dp2.RNS.GateType arg0) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Set<it.polito.dp2.RNS.GateReader> result_set = new HashSet<>();
+		if(arg0 != null){	// select all the gates from a given type specified in arg0
+			String type = arg0.toString();
+			List<GateReader_> gates = new ArrayList<GateReader_>(this.utility.g_map.values()); // get the whole list of gates casting the map into a list
+			for(GateReader_ gr:gates){ 															// for each gate in the list
+				if(gr.getType().toString().compareTo(type) == 0){ 								// if the type is the selected one by arg0
+					result_set.add(gr); 														// add the gate reader to the return_set
+				}
+			}
+		}else{	// otherwise select all the gates
+			result_set.addAll(this.utility.g_map.values());
+		}
+		return result_set;
 	}
 
 	@Override
@@ -38,7 +52,6 @@ public class RnsReader_ implements it.polito.dp2.RNS.RnsReader {
 		return null;
 	}
 
-	// DONE -- CHECK
 	@Override
 	public it.polito.dp2.RNS.PlaceReader getPlace(String arg0) {
 		PlaceReader_ p = this.utility.p_map.get(arg0);
@@ -47,7 +60,14 @@ public class RnsReader_ implements it.polito.dp2.RNS.RnsReader {
 
 	@Override
 	public Set<it.polito.dp2.RNS.PlaceReader> getPlaces(String arg0) {
-		return null;
+		Set<it.polito.dp2.RNS.PlaceReader> result_set = new HashSet<>();
+		if(arg0 != null){ // we have to select only places with a given prefix
+			Map<String,PlaceReader_> map = this.utility.getByPrefix(arg0);	// return a pruned map of places
+			result_set.addAll(map.values());								// add the pruned map in the result set
+		}else{	// otherwise add all the places stored in p_map in result set
+			result_set.addAll(this.utility.p_map.values());
+		}
+		return result_set;
 	}
 
 	@Override
--- src/it/polito/dp2/RNS/sol1/UtilityMap.java
+++ src/it/polito/dp2/RNS/sol1/UtilityMap.java
@@ -1,16 +1,18 @@
 package it.polito.dp2.RNS.sol1;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.NavigableMap;
 import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
-import javax.xml.datatype.XMLGregorianCalendar;
-
-
+import it.polito.dp2.RNS.sol1.jaxb.ConnectionType;
 import it.polito.dp2.RNS.sol1.jaxb.GateType;
 import it.polito.dp2.RNS.sol1.jaxb.ParkingAreaType;
 import it.polito.dp2.RNS.sol1.jaxb.PlaceType;
@@ -22,12 +24,12 @@ public class UtilityMap {
 	
 
 	  public Map<String,IdentifiedEntityReader_> i_map;
-	  public Map<String,PlaceReader_>	p_map;
+	  public NavigableMap<String,PlaceReader_>	p_map;
 	  public Map<String,RoadSegmentReader_> rs_map;
 	  public Map<String,ParkingAreaReader_> pa_map;
 	  public Map<String,GateReader_> g_map;
 	  public Map<String,VehicleReader_> v_map;
-	  //public List<PlaceReader_> p_list;
+	  public List<ConnectionReader_> c_list;
 	  
 	  /**
 	   * Constructor
@@ -35,11 +37,18 @@ public class UtilityMap {
 	   */
 	  public UtilityMap(RoadNavigationSystem rns) {
 		  
-		this.p_map = new HashMap<>();
+		// If we work with `TreeMap` we can use benefits of underlying tree data structure,
+		// and search with prefix with a O(log(N)) complexity.
+		// If we work with `HashMap` we can easily retrieve objects with the key 
+		// with a O(1) complexity
+		// Both `TreeMap` and `HashMap` have the same `Map` interface.
+		  
+		this.p_map = new TreeMap<>();
 		this.rs_map = new HashMap<>();
 		this.pa_map = new HashMap<>();
 		this.i_map = new HashMap<>();
 		this.v_map = new HashMap<>();
+		this.c_list = new ArrayList<>();
 		
 		// PLACES
 		for(PlaceType p :rns.getPlace()){	// for each place in the system
@@ -82,15 +91,23 @@ public class UtilityMap {
 			
 		}
 		
+		// CONNECTIONS
+		for(ConnectionType c: rns.getConnection()){ // for each connection in the system
+			PlaceReader_ from = this.p_map.get(c.getFrom());							// get the `from` in the p_map
+			PlaceReader_ to = this.p_map.get(c.getTo());								// get the `to` in the p_map
+			ConnectionReader_ connection = new ConnectionReader_(from, to);				// create a new ConnectionReader
+			this.c_list.add(connection);												// store the connection reader
+		}
+		
 		
 		// NEXT PLACES
-		for(PlaceType p:rns.getPlace()){					// for each place
-			PlaceReader_ tmp = this.p_map.get(p.getId());	// get the corresponding reader
-			List<String> strings = p.getNextPlace();		// get the list of further places
+		for(PlaceType p:rns.getPlace()){ // for each place in the system
+			PlaceReader_ tmp = this.p_map.get(p.getId());	// get the corresponding reader in p_map
+			List<String> strings = p.getNextPlace();		// get the list of further places from p
 			Set<PlaceReader_> nextHops = new HashSet<>();	// create an empty HashSet
-			for(String tmp2:strings){						// for each next place in the list
-				PlaceReader_ nextHop = this.p_map.get(tmp2);	// get the corresponding next hop-reader
-				nextHops.add(nextHop); 						// add this hop in the hash set
+			for(String tmp2:strings){						// for each next place in the list coming from p
+				PlaceReader_ nextHop = this.p_map.get(tmp2);	// get the corresponding next hop-reader from p_map
+				nextHops.add(nextHop); 							// add this hop in the hash set
 			}
 			tmp.setNextPlaces(nextHops); //update the `next place` Set for this element
 		}
@@ -98,10 +115,23 @@ public class UtilityMap {
 		
 		
 		//creating read-only collections
-        //this.placeToString = Collections.unmodifiableMap(this.placeToString);
-        this.p_map = Collections.unmodifiableMap(this.p_map);
-        //this.p_list = Collections.unmodifiableList(this.p_list);
+        this.p_map = Collections.unmodifiableNavigableMap(this.p_map);
+        this.rs_map = Collections.unmodifiableMap(this.rs_map);
+        this.pa_map = Collections.unmodifiableMap(this.pa_map);
+        this.g_map = Collections.unmodifiableMap(this.g_map);
+        this.v_map = Collections.unmodifiableMap(this.v_map);
+        this.c_list = Collections.unmodifiableList(this.c_list);
 
+	}
+	  
+	/**
+	 * This method is able to get a portion of map using a prefix
+	 * with a O(log(N)) complexity
+	 * @param prefix
+	 * @return Pruned map
+	 */
+	public SortedMap<String, PlaceReader_> getByPrefix( String prefix ) {
+	        return this.p_map.subMap( prefix, prefix + Character.MAX_VALUE );
 	}
 
 }
